c478eebc5fb98cafd1d0a48038a60c72181c8b06
Jinwoo Sung <july91.sung@gmail.com>
2019-06-02 05:00:42 +0000

    Build my world

--- project1/build/cmake_install.cmake
+++ project1/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sung/project1
+# Install script for directory: /home/workspace/project1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,11 +32,6 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
-# Is this installation the result of a crosscompile?
-if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sung/project1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/workspace/project1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
