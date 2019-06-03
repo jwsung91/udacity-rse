d9d1ff5d6d0cdedf37dc3bd3c9c4321e333508c0
samchi <jwsung91@gmail.com>
2019-06-03 23:29:29 +0900

    Go Chase It!

---
+++ project2/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/sung/git/Udacity_RoboticsEngineer/project2/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/sung/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/sung/catkin_ws/devel;/opt/ros/melodic" != "" else []
