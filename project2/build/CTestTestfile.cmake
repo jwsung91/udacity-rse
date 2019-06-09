a0f7f9cf9acfd24c8bf56a080c5c944b2df69e5c
jinwoo sung <jwsung91@gmail.com>
2019-06-09 16:21:10 +0900

    remove folders

--- project2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sung/git/Udacity_RoboticsEngineer/project2/src
-# Build directory: /home/sung/git/Udacity_RoboticsEngineer/project2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot")
--- project2/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sung/git/Udacity_RoboticsEngineer/project2/src/my_robot
-# Build directory: /home/sung/git/Udacity_RoboticsEngineer/project2/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
