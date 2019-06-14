905e391f1640d2945b651f7606ddfe5a6fab7954
Jinwoo Sung <jwsung91@gmail.com>
2019-06-14 14:33:02 +0000

    where am i

---
+++ project3/build/src/ball_chaser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src/UND_Robo/project3/src/src/ball_chaser
+# Build directory: /home/workspace/catkin_ws/src/UND_Robo/project3/build/src/ball_chaser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
