d4471dbc7accce4028c2afc1b8eaf27b1dde2890
jinwoo sung <jwsung91@gmail.com>
2019-06-29 01:17:53 +0900

    remove build files

--- project4/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sung/git/UND_Robo/project4/src
-# Build directory: /home/sung/git/UND_Robo/project4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot")
-subdirs("teleop_twist_keyboard")
--- project4/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sung/git/UND_Robo/project4/src/my_robot
-# Build directory: /home/sung/git/UND_Robo/project4/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
