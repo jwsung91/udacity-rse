fff3cbfd0badb84461dd681602a258081260cb26
samchirobot <samchi.robot@gmail.com>
2019-08-04 15:54:26 +0900

    project setting

---
+++ project5/README.md
@@ -0,0 +1,7 @@
+xterm 설치 필요
+
+sudo apt install xterm
+sudo apt install ros-melodic-openslam-gmapping
+sudo apt install ros-melodic-teleop-*
+
+
---
+++ project5/build/slam_gmapping/slam_gmapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samchi/workspace/UND_Robo/project5/src/slam_gmapping/slam_gmapping
+# Build directory: /home/samchi/workspace/UND_Robo/project5/build/slam_gmapping/slam_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
