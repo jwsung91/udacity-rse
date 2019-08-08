e7dd71e4e0fa5a3a715034cb6624fa4b393165e0
jinwoo sung <jwsung91@gmail.com>
2019-08-08 19:40:09 +0900

    added pick objects

---
+++ project5/build/pick_objects/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samchi/workspace/UND_Robo/project5/src/pick_objects
+# Build directory: /home/samchi/workspace/UND_Robo/project5/build/pick_objects
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
