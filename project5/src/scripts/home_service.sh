680fecd2b55649c7f16601115fc2fdc9323c0692
samchi.robot <samchi.robot@gmail.com>
2019-08-10 08:06:57 +0900

    Update home_service.sh
    solve path hardcoding issues

--- project5/src/scripts/home_service.sh
+++ project5/src/scripts/home_service.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Set catkin_ws path
-workspace=~/workspace/UND_Robo/project5
-
 ## deploy a turtlebot in my environment
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$workspace/src/map/jin.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../map/jin.world" &
 sleep 10
 
 ## localize the turtlebot
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$workspace/src/map/jin_world_map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/jin_world_map.yaml" &
 sleep 2
 
 ## observe the map in rviz (+ marker)
