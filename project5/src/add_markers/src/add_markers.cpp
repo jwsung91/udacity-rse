dae607e037b3ec8478a0c3cba48287040849c095
jinwoo sung <jwsung91@gmail.com>
2019-08-09 22:02:42 +0900

    modified for home service robot

--- project5/src/add_markers/src/add_markers.cpp
+++ project5/src/add_markers/src/add_markers.cpp
@@ -1,16 +1,23 @@
 #include <ros/ros.h>
+#include <cmath>
+
 #include <visualization_msgs/Marker.h>
 #include "nav_msgs/Odometry.h"
-#include "geometry_msgs/Pose2D.h"
+//#include "geometry_msgs/Pose2D.h"
 
-geometry_msgs::Pose2D current_pose;
+#define REACH_DIST 0.1
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+//geometry_msgs::Pose2D current_pose;
+
+float current_pose_x = 0.0;
+float current_pose_y = 0.0;
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr msg)
 {
-  current_pose.x = msg->pose.pose.position.x;
-  current_pose.y = msg->pose.pose.position.y;
+  current_pose_x = msg->pose.pose.position.x;
+  current_pose_y = msg->pose.pose.position.y;
+  ROS_INFO("x: %f, y: %f", current_pose_x, current_pose_y);
 }
-
 
 int main( int argc, char** argv )
 {
@@ -19,17 +26,20 @@ int main( int argc, char** argv )
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
-  bool reach = false;
-
+  bool reach_goal = false;
+  float dist_x = 0;
+  float dist_y = 0;
 
   // add odom subscriber
-  ros::Subscriber odom_sub = n.subscribe("/odom", 10, odomCallback);
+  ros::Subscriber odom_sub = n.subscribe("/odom", 1000, odomCallback);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
   // Set Phase (modified)
   uint8_t phase = 0;
+
+  float goals[2][3] = {{5,5,0},{1,-3,0}};
 
   while (ros::ok())
   {
@@ -46,39 +56,6 @@ int main( int argc, char** argv )
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
     marker.type = shape;
 
-    switch (phase)
-    {
-        case 1:
-            ROS_INFO("phase 1 : Publish the marker at the pickup zone and wait 5 seconds");
-            marker.action = visualization_msgs::Marker::ADD;
-            marker.pose.position.x = 5;
-            marker.pose.position.y = 5;
-            marker.pose.position.z = 0;
-            marker.pose.orientation.x = 0.0;
-            marker.pose.orientation.y = 0.0;
-            marker.pose.orientation.z = 0.0;
-            marker.pose.orientation.w = 1.0;
-        break;
-        
-        case 2:
-            ROS_INFO("phase 2 : Hide the marker and wait 5 seconds");
-            marker.action = visualization_msgs::Marker::DELETE;
-        break;
-        
-        case 3:
-            ROS_INFO("phase 3 : Publish the marker at the drop off zone");
-            marker.action = visualization_msgs::Marker::ADD;
-            marker.pose.position.x = 1;
-            marker.pose.position.y = -3;
-            marker.pose.position.z = 0;
-            marker.pose.orientation.x = 0.0;
-            marker.pose.orientation.y = 0.0;
-            marker.pose.orientation.z = 0.0;
-            marker.pose.orientation.w = 1.0;
-            break;
-    }
-    phase++;
-    
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 0.3;
@@ -103,10 +80,60 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
+
+    while(1)
+    {
+      if(phase==0)
+      {
+        phase=1;
+        break;
+      }
+      dist_x = fabs(current_pose_x-goals[phase-1][0]);
+      dist_y = fabs(current_pose_x-goals[phase-1][1]);
+
+      if(dist_x<=REACH_DIST&&dist_y<=REACH_DIST)
+      {
+        reach_goal=true;
+        phase++;
+      }
+    }
+    switch (phase)
+    {
+        case 1:
+            ROS_INFO("phase 1 : Publish the marker at the pickup zone and wait 5 seconds");
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = goals[0][0];
+            marker.pose.position.y = goals[0][1];
+            marker.pose.position.z = goals[0][2];
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+        break;
+        
+        case 2:
+            ROS_INFO("phase 2 : Hide the marker and wait 5 seconds");
+            marker.action = visualization_msgs::Marker::DELETE;
+        break;
+        
+        case 3:
+            ROS_INFO("phase 3 : Publish the marker at the drop off zone");
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = goals[1][0];
+            marker.pose.position.y = goals[1][1];
+            marker.pose.position.z = goals[1][2];
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            break;
+    }
+
     marker_pub.publish(marker);
-    sleep(5);
+
     if(phase>3)
-        return 0;
+        while(1)
+        {}
     r.sleep();
   }
 }
